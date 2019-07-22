fd75e31b43417664dff692252d695244d7e4efe7
Jackson Powell <jpowell@lrtico.com>
2019-07-22 08:26:18 -0700

    Update index.js
    Updated port to 5000 in order for it to work with the placeholder data from the App.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const pdfTemplate = require("./documents/");
 
 //initialize the app and set up the port
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 //set up middleware
 app.use(cors());
